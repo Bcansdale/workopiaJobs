f93a98c504d7e58f22eedb2754fea35d514c1de2
Bcansdale <28786879+Bcansdale@users.noreply.github.com>
2025-11-04 14:53:44 -0700

    routes and constaints

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Route; // Global constraint
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+//        Route::pattern('id', '[0-9]+'); // Global constraint for routes
+//        Route::pattern('comment', '[a-zA-Z]'); // Global constraint for routes
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,28 +10,36 @@ Route::get('/jobs', function () {
     return 'Available Jobs';
 })->name('jobs'); // naming route convention
 
-Route::post('submit', function () {
-    return 'Submitted';
-});
+Route::get('/posts/{id}', function (string $id) {
+    return 'Post' . $id;
+})->where('id', '[0-9]+');
 
-Route::match(['get', 'post'], '/submit', function () {
-    return "Submitted";
-});
+Route::get('/posts/{id}/comments/{commentId}', function (string $id, string $commentId) {
+    return 'Post: ' . $id . ' Comment: ' . $commentId;
+})->where('id', '[0-9]+')->where('comment', 'a-zA-Z');
 
-Route::any('/submit', function () {
-    return "Submitted";
-});
-
-Route::get('/test', function () {
-    $url = route('jobs');
-    return "<a href='$url'>Click here for jobs</a>";
-});
-
-Route::get('/api/users', function () {
-    return [
-        'name' => 'John Doe',
-        'email' => 'John@gmail.com'
-    ];
-});
+//Route::post('submit', function () {
+//    return 'Submitted';
+//});
+//
+//Route::match(['get', 'post'], '/submit', function () {
+//    return "Submitted";
+//});
+//
+//Route::any('/submit', function () {
+//    return "Submitted";
+//});
+//
+//Route::get('/test', function () {
+//    $url = route('jobs');
+//    return "<a href='$url'>Click here for jobs</a>";
+//});
+//
+//Route::get('/api/users', function () {
+//    return [
+//        'name' => 'John Doe',
+//        'email' => 'John@gmail.com'
+//    ];
+//});
 
 // brandon in laravelStarter/workopia on branch  $ php artisan route:list will show all routes

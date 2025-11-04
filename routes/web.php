34caf80d1f54f1a5df7dea2764525cf9566dff75
Bcansdale <28786879+Bcansdale@users.noreply.github.com>
2025-11-04 12:09:43 -0700

    web.php removed again...

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,35 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Welcome');
 });
+
+Route::get('/jobs', function () {
+    return 'Available Jobs';
+})->name('jobs'); // naming route convention
+
+Route::post('submit', function () {
+    return 'Submitted';
+});
+
+Route::match(['get', 'post'], '/submit', function () {
+    return "Submitted";
+});
+
+Route::any('/submit', function () {
+    return "Submitted";
+});
+
+Route::get('/test', function () {
+    $url = route('jobs');
+    return "<a href='$url'>Click here for jobs</a>";
+});
+
+Route::get('/api/users', function () {
+    return [
+        'name' => 'John Doe',
+        'email' => 'John@gmail.com'
+    ];
+});
+
+// brandon in laravelStarter/workopia on branch  $ php artisan route:list will show all routes
